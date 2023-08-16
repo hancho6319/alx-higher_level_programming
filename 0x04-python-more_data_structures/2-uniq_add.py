f64a4b3148990d197835badf24ba78b062a69e6d
hancho6319 <abbaauwalharuna@gmail.com>
2023-08-16 21:51:41 +0000

    my commit

--- 0x04-python-more_data_structures/2-uniq_add.py
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
-
 def uniq_add(my_list=[]):
-    new_list = []
-    for ele in range(len(my_list)):
-        new_list[ele] = my_list[ele]
-        for i in my_list:
-            if 
+    uniq_list = set(my_list)
+    num = 0
+
+    for i in uniq_list:
+        num += i
+
+    return (num)
