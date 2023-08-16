beda7cf51c5a9729851127c55960be145c971757
hancho6319 <abbaauwalharuna@gmail.com>
2023-08-16 09:23:31 +0000

    my commit

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,11 +1,9 @@
 #!/usr/bin/python3
 
-def square_matrix_simple(matrix = []):
+def square_matrix_simple(matrix=[]):
     new_matrix = matrix.copy()
 
     for ls in range(len(matrix)):
-        new_matrix[ls] = list(map(lambda value : value**2, matrix[ls]))
+        new_matrix[ls] = list(map(lambda value: value**2, matrix[ls]))
 
     return new_matrix
-        
-
