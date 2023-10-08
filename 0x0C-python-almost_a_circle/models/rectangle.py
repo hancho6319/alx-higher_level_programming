d9c536de686c06aab053ccf45cb59ad027b3ba0c
hancho6319 <abbaauwalharuna@gmail.com>
2023-10-08 09:01:04 +0000

    my commit

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -24,10 +24,10 @@ class Rectangle(Base):
         """
 
         super().__init__(id)
-        self.__width = width
-        self.__height = height
-        self.__x = x
-        self.__y = y
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
 
     @property
     def width(self):
@@ -77,7 +77,7 @@ class Rectangle(Base):
         """ This is the method used to set any value to the private __x """
         if not isinstance(x_value, int):
             raise TypeError("x must be an integer")
-        if x_value <= 0:
+        if x_value < 0:
             raise ValueError("x must be >= 0")
         self.__x = x_value
 
@@ -96,7 +96,7 @@ class Rectangle(Base):
         """ This is the method used to set any value to the private __y """
         if not isinstance(y_value, int):
             raise TypeError("y must be an integer")
-        if y_value <= 0:
+        if y_value < 0:
             raise ValueError("y must be >= 0")
         self.__y = y_value
 
@@ -106,4 +106,67 @@ class Rectangle(Base):
         return self.__y
     
     def area(self):
-        return self.__height * self.__width
+        """ This is the method used to return the area of the rectangle """
+        return self.height * self.width
+    
+    def display(self):
+        """ This is the method used to return the n times of # """
+        [print("") for y in range(self.y)]
+        for h in range(self.height):
+            [print(" ", end="") for x in range(self.x)]
+            [print("#", end="") for w in range(self.width)]
+            print("")
+
+    def __str__(self) -> str:
+        """ This is overriding the __str__  method"""
+        return "[Rectangle] ({}) {}/{} - {}/{}".format(self.id,
+                                                        self.x, self.y,
+                                                        self.width, self.height)
+        
+    def update(self, *args, **kwargs):
+        """ Update the Rectangle.
+        
+        Args:
+        *args (ints): New attribute values.
+            - 1st argument represents id attribute
+            - 2nd argument represents width attribute
+            - 3rd argument represent height attribute
+            - 4th argument represents x attribute
+            - 5th argument represents y attribute
+        **kwargs (dict): New key/value pairs of attributes.
+        """
+        if args and len(args) != 0:
+            a = 0
+            for arg in args:
+                if a == 0:
+                    if arg is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = arg
+                elif a == 1:
+                    self.width = arg
+                elif a == 2:
+                    self.height = arg
+                elif a == 3:
+                    self.x = arg
+                elif a == 4:
+                    self.y = arg
+            a += 1
+
+        elif kwargs and len(kwargs) != 0:
+            for k, v in kwargs.items():
+                if k == "id":
+                    if v is None:
+                        self.__init__(self.width, self.height, self.x, self.y)
+                    else:
+                        self.id = v
+                elif k == "width":
+                    self.width = v
+                elif k == "height":
+                    self.height = v
+                elif k == "x":
+                    self.x = v
+                elif k == "y":
+                    self.y = v
+
+    #def
---
+++ 0x0C-python-almost_a_circle/models/square.py
@@ -0,0 +1,32 @@
+#!/usr/bin/python3
+""" This is the defination of square class """
+from rectangle import Rectangle
+
+class Square(Rectangle):
+    """ This is the defination of the square class """
+
+    def __init__(self, size, x=0, y=0, id=None):
+        """ This is the initialization constructor of the class
+        
+        Args:
+            size (int): This is the size value
+            x (int): This is the x value
+            y (int): This is the y value
+            id (int): This is the id value
+        """
+        super().__init__(size, size, x, y, id)
+
+    def __str__(self):
+        """ This is the overloading __str__ method """
+        return f"[Square] ({self.id}) {self.x}/{self.y} - {self.size}"
+    
+    @property
+    def size(self):
+        """ This is the decorator used for size """
+        return self.width * self.height
+    
+    @size.setter
+    def size(self, widthAndHeight_value):
+        """ This is the setter of the size method """
+        self.height = widthAndHeight_value
+        self.width = widthAndHeight_value
